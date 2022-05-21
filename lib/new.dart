899eb9a3c2c278817aaf564a7f6255c5d8933ea4
sudhanshujuyal <shudhanshu@pearlorganisation.com>
2022-05-21 20:37:00 +0530

    new dart class

---
+++ lib/new.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class NewDart extends StatefulWidget {
+  const NewDart({Key? key}) : super(key: key);
+
+  @override
+  State<NewDart> createState() => _NewDartState();
+}
+
+class _NewDartState extends State<NewDart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
